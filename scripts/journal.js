96cc94471cefee21950cf8ad523548bc9fc5056f
sncandiani <sofiancandiani@gmail.com>
2020-01-22 23:42:36 -0600

    created an api and made changes to js to include api fetch

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,55 +1,33 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntry = {
-    date: "January 19 2020",
-    concepts: "Javascript functions, objects, and more!",
-    comments: "Javascript is a fun, but intimidating language..can't wait to learn more!",
-    mood: "Excited"
-}
-
-const journalEntries = {
-    entry: []
-}
-
-journalEntries.entry.push(journalEntry)
-console.log(journalEntries)
-
-const moreLearned = {
-    learned: ["Working in a group", "Git commands"]
-}
-
-journalEntries.entry.push(moreLearned)
-console.log(journalEntries)
-
-/*
-    Purpose: To create, and return, a string template that
-    represents a single journal entry object as HTML
-
-    Arguments: journalEntry (object)
-*/
+//create a container for entries
 const journalContainer = document.querySelector(".entryLog")
-
+//make html
 const makeJournalEntryComponent = (journalEntries) => {
     // Create your own HTML structure for a journal entry
     return `
-        <h1>${journalEntry.date}</h1>
-        <h2>${journalEntry.concepts}</h2>
-        <h3>${journalEntry.comments}</h3>
-        <h4>${journalEntry.mood}</h4>
+        <h1>${journalEntries.date}</h1>
+        <h2>${journalEntries.concepts}</h2>
+        <h3>${journalEntries.comments}</h3>
+        <h4>${journalEntries.mood}</h4>
     `
 }
-
-/*
-    Purpose: To render all journal entries to the DOM
-
-    Arguments: entries (array of objects)
-*/
+//render html to DOM
 const renderJournalEntries = (entries) => {
     const entryInfo = makeJournalEntryComponent(entries)
     journalContainer.innerHTML += entryInfo
 }
 
-// Invoke the render function
-renderJournalEntries(journalEntries)
+//fetch api (entries.json)
+const url = "http://localhost:3000/entries"
+fetch(url) // Fetch from the API
+    .then(resp => resp.json())  // Json Parse
+    .then(entries => {
+        // Loop through each entry
+        entries.forEach(entry => {
+            
+            const entryHtml = makeJournalEntryComponent(entry); //Make html for each entry
+            journalContainer.innerHTML += entryHtml //Append the html to the dom
+        })
+
+    })
+
+    
