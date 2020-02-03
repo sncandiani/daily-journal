1e39c8498d2fbe6a148d2e55768988c0484ebf4f
sncandiani <sofiancandiani@gmail.com>
2020-02-03 15:24:31 -0600

    added chaining .then promises

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,6 +1,6 @@
 const API = {
     getJournalEntries () {
-        return fetch("http://localhost:3000/entries")
+        return fetch("http://localhost:8088/entries")
             .then(response => response.json())
     }
 }
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,4 +1,52 @@
 import API from "./data.js"
 import renderJournalEntries from "./entriesDOM.js"
 
+
 API.getJournalEntries().then(renderJournalEntries)
+
+const journalDate = document.getElementById("journalDate")
+const journalConcepts = document.getElementById("journalText")
+const journalComments = document.getElementById("journalTextArea")
+const journalMood = document.getElementById("journalSelect")
+const newJournalEntry = (date, concepts, comments, mood) => {
+    return {
+        "date": date,
+        "concepts": concepts,
+        'comments': comments,
+        "mood": mood
+    }
+}
+
+const journalArray = [journalDate, journalConcepts, journalComments, journalMood]
+
+const addClickEvent = {
+    addSaveEventListener() {
+        const button = document.getElementById("recordJournalButton")
+        /* console.log(button) */
+
+
+        button.addEventListener("click", (e) => {
+            journalArray.forEach(entry => {
+                if(entry.value === "" || entry.value === "Please Select") {
+                    console.log("Entry needs information")
+                }
+                else {
+
+                    return fetch("http://localhost:8088/entries", { // Replace "url" with your API's URL
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newJournalEntry(`${journalDate.value}`, `${journalConcepts.value}`, `${journalComments.value}`, `${journalMood.value}`))
+            })
+                .then(API.getJournalEntries)
+                .then(renderJournalEntries)
+
+                }
+            })
+
+        })
+    }
+}
+
+addClickEvent.addSaveEventListener()
