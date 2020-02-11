d5b3255f9314c033525473db30ccbada0fa0537b
sncandiani <sofiancandiani@gmail.com>
2020-02-11 09:03:23 -0600

    added search bar

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,12 +1,14 @@
 const makeJournalEntryComponent = (journalEntries) => {
     // Create your own HTML structure for a journal entry
     return `
+    <section>
         <h1>${journalEntries.date}</h1>
         <h2>${journalEntries.concepts}</h2>
         <h3>${journalEntries.comments}</h3>
         <h4>${journalEntries.mood}</h4>
         <button id="deleteButton--${journalEntries.id}">Delete</button>
         <button id="editButton--${journalEntries.id}">Edit</button>
+    </section>
     `
 }
 
